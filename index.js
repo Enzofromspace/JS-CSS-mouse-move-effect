a3d8a47e386b58b3af2cf83ccaf0c5c562ace9ff
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-05 11:00:32 +0300

    Finished - can add more next

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function shadow(e){
     y = y + e.target.offsetTop;
   }
   const xWalk = Math.round((x / width * walk) - (walk / 2)); // changes from 50 to -1 instead of counting from 0 - 100
-  const yWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / width * walk) - (walk / 2));
 
   text.style.textShadow = `${xWalk}px ${yWalk}px 0 red`;
 }
